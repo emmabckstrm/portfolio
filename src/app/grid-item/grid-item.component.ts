5c078396562ecc3cc3c607ee3f128d8465b144f6
Emma Bäckström <backstromemma@gmail.com>
2017-02-06 21:01:49 +0100

    Menu and restructuring of layout + bulma

--- src/app/grid-item/grid-item.component.ts
+++ src/app/grid-item/grid-item.component.ts
@@ -4,7 +4,7 @@ import { PortfolioItem } from '../portfolio-item';
 @Component({
   selector: 'masonry-brick',
   templateUrl: './grid-item.component.html',
-  styleUrls: ['./grid-item.component.css']
+  styleUrls: ['./grid-item.component.scss']
 })
 export class GridItemComponent implements OnInit {
 
